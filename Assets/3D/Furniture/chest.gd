f5f4d64a1a62f63be8a1a4ace0e804846835b212
CosmicCoincidence <cosmiccoincidence1991@gmail.com>
2025-12-28 04:01:23 -0500

    make chest_level dependant on map

--- Assets/3D/Furniture/chest.gd
+++ Assets/3D/Furniture/chest.gd
@@ -1,8 +1,9 @@
 extends BaseFurniture
 class_name Chest
 
-# NEW LOOT SYSTEM
-@export var chest_level: int = 5  # Determines item levels dropped
+# LEVEL-BASED LOOT SYSTEM
+@export var base_chest_level: int = 0  # Base level offset (0 = normal, +1 = better loot, etc.)
+@export var chest_level: int = 5  # Final calculated level (map_level + base_chest_level)
 @export var loot_profile: LootProfile  # Profile for this chest type
 
 @export var open_sound: AudioStream
@@ -51,6 +52,11 @@ func _ready():
 		audio_player.name = "AudioStreamPlayer3D"
 		add_child(audio_player)
 
+# Called by map generator to set chest level based on map level
+func set_level_from_map(map_level: int):
+	chest_level = map_level + base_chest_level
+	print("Chest set to level ", chest_level, " (map: ", map_level, " + base: ", base_chest_level, ")")
+
 func _physics_process(_delta):
 	if not is_open and check_interaction():
 		open_chest()
@@ -73,7 +79,7 @@ func open_chest():
 	# Spawn loot using new system
 	spawn_loot()
 	
-	print("Chest opened!")
+	print("Chest opened! (Level ", chest_level, ")")
 
 func spawn_loot():
 	if not loot_profile:
